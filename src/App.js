eeaa5a57a33908e0da18ab88d480493fc2bd66e8
Arifitanto Oktarian Hafayer <arifitantotito@gmail.com>
2023-04-21 11:05:02 +0700

    Landing Page And About Done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Landing from './Components/Landing/Landing';
+import About from './Components/About/About';
+
+function App() {
+  
+  return (
+    <>
+      <div className='bg-black'>
+        <Landing/>
+        <About/>
+      </div>
+    </>
+  );
+}
+
+export default App;
